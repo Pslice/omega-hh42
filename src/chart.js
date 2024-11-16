7198c660c466e6f7db2e56b57c9800eb07022eb7
Patrick Cool <patrickscool@gmail.com>
2024-11-15 22:47:04 -0800

    update

---
+++ src/chart.js
@@ -0,0 +1,31 @@
+console.log('this is the chart');
+
+let chartData = {
+    y: [],
+    type: 'line',
+    name: 'data'
+};
+
+// Initialize the plot
+Plotly.newPlot('chart', [chartData]);
+
+let counter = 0;
+
+// Listen for serial data
+window.API.onSerialData((data, unit) => {
+    // Extend the chart with new data
+    Plotly.extendTraces('chart', {
+        y: [[data]]
+    }, [0]);
+
+    counter++;
+
+    // Keep only last 50 points visible
+    if (counter > 50) {
+        Plotly.relayout('chart', {
+            xaxis: {
+                range: [counter - 50, counter]
+            }
+        });
+    }
+});
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
+const OmegaHH42 = require('./device/hh42');
+
+let hh42;
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  hh42 = new OmegaHH42(mainWindow);
+
+  mainWindow.webContents.openDevTools();
+};
+
+
+app.whenReady().then(() => {
+  createWindow();
+  ipcMain.handle(`getTemperaturePorts`, async () => {
+    return await OmegaHH42.getAvailablePorts();
+  });
+  setupIpcHandlersTemperature();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+function setupIpcHandlersTemperature() {
+  ipcMain.on('updateTemperaturePort', (event, portName) => {
+    hh42.initializeSerialPort(portName);
+  });
+}
+// function setupIpcHandlersTemperature() {
+//   ipcMain.on('updateTemperaturePort', (event, portName) => {
+//     hh42.initializeSerialPort(portName);
+//   });
+// }
